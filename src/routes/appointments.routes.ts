1cb68a156863d9e631e0a07ef4b45d25eedd0f89
Gabriel Ferrari <gabrieljuniorferrari88@gmail.com>
2022-09-06 20:50:28 -0300

    docs: entendo um pouco sobre o proje a ser desenvolvido e rotas usando express

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,38 @@
+import { Router } from 'express'
+import { uuid } from 'uuidv4'
+import { startOfHour, parseISO, isEqual } from 'date-fns'
+
+const appointmentsRouter = Router()
+
+interface Appointment {
+  id: string
+  provider: string
+  date: Date
+}
+
+const appointments: Appointment[] = []
+
+appointmentsRouter.post('/', (req, res) => {
+  const { provider, date } = req.body
+
+  const parsedDate = startOfHour(parseISO(date))
+  const findAppointmentInSameDate = appointments.find((appointment) =>
+    isEqual(parsedDate, appointment.date),
+  )
+
+  if (findAppointmentInSameDate) {
+    return res.status(400).json({ message: 'Esse horário já esta agendado!' })
+  }
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date: parsedDate,
+  }
+
+  appointments.push(appointment)
+
+  return res.json({ message: appointment })
+})
+
+export default appointmentsRouter
