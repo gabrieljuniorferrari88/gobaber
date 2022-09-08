d4627454295c7354c8ede4d3717f99833fd2df1a
Gabriel Ferrari <gabrieljuniorferrari88@gmail.com>
2022-09-08 16:02:30 -0300

    feature: criando rota para file para exibir nossos avatar

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,7 @@
 import express from 'express'
-
 import AppDataSource from './dataSource'
-
 import routes from './routes'
+import uploadConfig from './config/upload'
 
 AppDataSource.initialize()
   .then(() => {
@@ -11,6 +10,7 @@ AppDataSource.initialize()
     const app = express()
 
     app.use(express.json())
+    app.use('/files', express.static(uploadConfig.directory))
     app.use(routes)
 
     app.listen(process.env.PORT, () => {
